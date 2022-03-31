3cda54ad1c9593bd43b19bb65322cba62b0c8e41
amberdevpe <amberdevpe@gmail.com>
2022-03-30 23:56:29 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className='container py-5'>
+      <div className='row'>
+        <div className='col-md-6'>
+          <div className='card'>
+            <div className='card-header'>
+              <h3>Hello Dojo!</h3>
+              <h6>Things I need to do:</h6>
+            </div>  
+            <div className='card-body'>
+              <ul className='list-group'>
+                <li className='list-group-item'>Learn React</li>
+                <li className='list-group-item'>Climb Mt. Everest</li>
+                <li className='list-group-item'>Run a marathon</li>
+                <li className='list-group-item'>Feed the dogs</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+   
   );
 }
 
